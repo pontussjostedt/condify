efdcaa937b651891b1103a7de4e3e331598d41ec
Pontus Sjostedt <po7853sj-s@student.lu.se>
2023-12-22 11:34:39 +0100

    clean imports

--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -1,11 +1,1 @@
-use std::marker;
 
-use nom::branch::alt;
-use nom::bytes::complete::*;
-use nom::character::complete::{anychar, crlf, newline, space0};
-use nom::combinator::*;
-use nom::error::{context, VerboseError};
-use nom::multi::{many0, separated_list0};
-use nom::sequence::{delimited, preceded, terminated, tuple};
-use nom::IResult;
-//type ParserType<'a> = FnMut(&str)
