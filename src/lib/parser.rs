b74ea232d1d8a6979ebe756dab572f80adc8babd
Pontus Sjostedt <po7853sj-s@student.lu.se>
2023-12-22 11:39:03 +0100

    tokentype start

--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -1,1 +1,13 @@
+use nom_locate::LocatedSpan;
 
+type Span<'a> = LocatedSpan<&'a str>;
+
+struct AssignmentArm<'a> {
+    include: Vec<&'a str>,
+    value: &'a str,
+}
+enum Token<'a> {
+    Declare(Span<'a>, Vec<&'a str>),
+    Assignment(Span<'a>, AssignmentArm<'a>),
+    FreeText(Span<'a>, &'a str),
+}
