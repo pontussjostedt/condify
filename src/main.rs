2a6fda49cd7e958a749b5e3eb23d3d9b2b4a75bb
Pontus Sjostedt <po7853sj-s@student.lu.se>
2024-02-04 13:52:13 +0100

    add some tests for target output

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use clap::Parser;
+use itertools::Itertools;
 use lib::dsl_parser::parse;
 use nom::error::convert_error;
 use std::{
     collections::HashMap,
     fs::{self, File},
     io::{self, Read, Write},
+    path::PathBuf,
+    process::Output,
 };
 
 use crate::lib::{
@@ -21,11 +24,31 @@ struct Cli {
     #[arg(short, long)]
     output: Option<std::path::PathBuf>,
 
-    #[arg(short, long)]
-    suffix: Option<std::path::PathBuf>,
-
+    //#[arg(short, long)]
+    //suffix: Option<std::path::PathBuf>,
     #[arg(long)]
     watch: bool,
+}
+
+impl Cli {
+    fn target_output(&self) -> PathBuf {
+        self.output.clone().unwrap_or_else(|| {
+            let input_path = self.input.to_str().expect("Input is not valid unicode");
+            let mut out = String::new();
+
+            if let Some(first_dot_index) = input_path.find('.') {
+                let (prefix, suffix) = input_path.split_at(first_dot_index);
+                out.push_str(prefix);
+                out.push_str("%s.");
+                out.push_str(&suffix[1..]); // Skip the dot
+            } else {
+                out.push_str(input_path);
+                out.push_str("%s");
+            }
+
+            PathBuf::from(out)
+        })
+    }
 }
 
 fn get_output_map<'a>(input: &'a str) -> Result<HashMap<String, String>, String> {
@@ -42,22 +65,18 @@ fn get_output_map<'a>(input: &'a str) -> Result<HashMap<String, String>, String>
     Ok(output)
 }
 
-fn write_hashmap(
-    to_write: HashMap<String, String>,
-    target: std::path::PathBuf,
-    suffix: &str,
-) -> io::Result<()> {
+fn write_hashmap(to_write: HashMap<String, String>, target: std::path::PathBuf) -> io::Result<()> {
     for (key, value) in to_write {
-        let path = target.join(key).join(suffix);
-        fs::File::create(path)?.write(value.as_bytes())?;
+        //let path = target.join(key).join(suffix);
+        //fs::File::create(path)?.write(value.as_bytes())?;
     }
     Ok(())
 }
 
-fn do_try_write<'a>(input: &'a str, target: std::path::PathBuf, suffix: &'a str) -> io::Result<()> {
+fn do_try_write<'a>(input: &'a str, target: PathBuf) -> io::Result<()> {
     let result = get_output_map(input);
     match result {
-        Ok(to_write) => write_hashmap(to_write, target, suffix)?,
+        Ok(to_write) => write_hashmap(to_write, target)?,
         Err(error_msg) => println!("{}", error_msg),
     }
     Ok(())
@@ -69,6 +88,74 @@ fn main() -> io::Result<()> {
         File::open(&cli.input).expect(&format!("Could not read cli.input: {:?}", cli.input));
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+    println!("{:?}", "Hello".split(".").collect_vec());
     while cli.watch {}
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+    #[test]
+    fn test_insert_branch_name_no_output_one_dot() {
+        let cli = Cli {
+            input: PathBuf::from_str("text.txt").unwrap(),
+            output: None,
+            watch: false,
+        };
+
+        let expected_output = PathBuf::from_str("text%s.txt").unwrap();
+
+        assert_eq!(cli.target_output(), expected_output);
+    }
+
+    #[test]
+    fn test_insert_branch_name_no_output_many_dots() {
+        let cli = Cli {
+            input: PathBuf::from_str("text.txt.png").unwrap(),
+            output: None,
+            watch: false,
+        };
+
+        let expected_output = PathBuf::from_str("text%s.txt.png").unwrap();
+
+        assert_eq!(cli.target_output(), expected_output);
+    }
+
+    #[test]
+    fn test_insert_branch_name_no_output_no_dots() {
+        let cli = Cli {
+            input: PathBuf::from_str("text").unwrap(),
+            output: None,
+            watch: false,
+        };
+
+        let expected_output = PathBuf::from_str("text%s").unwrap();
+
+        assert_eq!(cli.target_output(), expected_output);
+    }
+
+    #[test]
+    fn test_insert_branch_name_with_output() {
+        let cli = Cli {
+            input: PathBuf::from_str("text.txt").unwrap(),
+            output: Some(PathBuf::from_str("output%s.txt").unwrap()),
+            watch: false,
+        };
+
+        assert_eq!(cli.target_output(), cli.output.unwrap());
+    }
+
+    #[test]
+    fn test_insert_branch_name_with_invalid_output() {
+        let cli = Cli {
+            input: PathBuf::from_str("text.txt").unwrap(),
+            output: Some(PathBuf::from_str("output.txt").unwrap()),
+            watch: false,
+        };
+
+        assert_eq!(cli.target_output(), cli.output.unwrap());
+    }
 }
